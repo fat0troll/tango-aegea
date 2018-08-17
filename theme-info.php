dc1ea9a59bcea0ac26709dc7e9617c39ddfad2cf
Vladimir Hodakov <vladimir@hodakov.me>
2018-08-17 17:07:15 +0400

    Initial commit

---
+++ theme-info.php
@@ -0,0 +1,20 @@
+<?php return array (
+
+  'index' => 9,
+
+  'display_name' => array (
+    'en' => 'Tango',
+    'ru' => 'Танго',
+  ),
+
+  'colors' => array (
+    'background' => '#2e3436',
+    'headings' => '#babdb6',
+    'text' => '#babdb6',
+    'link' => '#729fcf',
+  ),
+
+  'based_on' => 'plain',
+  'meta_viewport' => 'width=device-width, initial-scale=1',
+
+); ?>
